fd66ddc4085bc79efc97bf4c4918e0059967844e
Vikrant Singh <jadounvikrant@gmail.com>
2026-07-31 23:53:22 +0530

    Simple question

--- Day11 (JavaScript)/script.js
+++ Day11 (JavaScript)/script.js
@@ -1,54 +1,59 @@
-console.log("Starting JS!")
+// console.log("Starting JS!")
 
-// words vs keyword -> words which are reserved and can not be used as variable
+// // words vs keyword -> words which are reserved and can not be used as variable
 
-//variables and declaration 
+// //variables and declaration 
 
-// var, let, const
-
-
-var ab = 12
-var ab = 56 // show no error weven a is already declare and initization
-
-//===================================//
-
-let b =23;
-//let b =28; //-> Give SyntaxError as b is already declared
-
-const discount = 32 // => can not change
+// // var, let, const
 
 
-// =====Scope (block, functional, global)
+// var ab = 12
+// var ab = 56 // show no error weven a is already declare and initization
 
-function abcd(){
-    if(true){
-        var a  =45;   // Functional scope is like it is accessable to nearest parent (var is accessable) in this case is function abcd()
-    }
-}
+// //===================================//
 
-//Temporal Dead Zone
+// let b =23;
+// //let b =28; //-> Give SyntaxError as b is already declared
+
+// const discount = 32 // => can not change
 
 
-// console.log(a)
-// let a =45;
+// // =====Scope (block, functional, global)
 
-//================================================//
+// function abcd(){
+//     if(true){
+//         var a  =45;   // Functional scope is like it is accessable to nearest parent (var is accessable) in this case is function abcd()
+//     }
+// }
 
-//hoisting impact per type
+// //Temporal Dead Zone
 
-//hoisting -> ek variable kab banta hai to vo do hisso mei tu jaata hai and uska declare wala part upar chala jaata hai aur inititlization wala neeche rah jaata hai
 
-// Example var name = "Vikrant";
+// // console.log(a)
+// // let a =45;
 
-console.log(name)
+// //================================================//
 
-var name = 45;
+// //hoisting impact per type
 
-console.log(h1)
-let h1 = 12;
+// //hoisting -> ek variable kab banta hai to vo do hisso mei tu jaata hai and uska declare wala part upar chala jaata hai aur inititlization wala neeche rah jaata hai
+
+// // Example var name = "Vikrant";
+
+// console.log(name)
+
+// var name = 45;
+
+// console.log(h1)
+// let h1 = 12;
 
 // var -> hoisting -> undefned
 //let -> hoisting -> X
 //const -> hoisting -> X
 
 
+// common Confusion
+
+console.log(name);
+
+var name = 45;
