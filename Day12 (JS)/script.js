e3360a0370f2d78bf739fe52f97513b6894cf41d
VJADOUN2000 <jadounvikrant@gmail.com>
2026-08-01 22:15:13 +0530

    primitives data

--- Day12 (JS)/script.js
+++ Day12 (JS)/script.js
@@ -1,12 +1,20 @@
 // Some basic scope questions
 
-let a =10;
+let ab =10;
 {
-    let a = 20;
-    console.log("inside:", a)
+    let ab = 20;
+    console.log("inside:", ab)
 }
 
-console.log(a);
+console.log(ab);
 
 //Data Types + Type System
 
+// primitives - aise value jisko copy karne par tumhe ek real copy mil jaaye
+// string, number, boolean, null, undefined, symbol, bigInt
+
+let a =45;
+
+
+//non-primitives (reference) -> inko copy karne par reference value milte hai naki real value
+// array, objects 
