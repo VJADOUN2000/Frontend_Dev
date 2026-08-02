176e38ef229288ab2c465f0fe7a701ce00c98b55
Vikrant Singh <jadounvikrant@gmail.com>
2026-08-02 23:19:11 +0530

    understanding primitive changes

--- Day12 (JS)/script.js
+++ Day12 (JS)/script.js
@@ -13,8 +13,49 @@ console.log(ab);
 // primitives - aise value jisko copy karne par tumhe ek real copy mil jaaye
 // string, number, boolean, null, undefined, symbol, bigInt
 
-let a =45;
+//let a =45;
 
 
 //non-primitives (reference) -> inko copy karne par reference value milte hai naki real value
-// array, objects 
+// array, objects functions
+
+//[], {}, ()
+
+//1 String
+
+// "" -> double quotes, '' -> single quotes, `` -> backticks
+
+//2. Number 
+
+// let a = 12 , 15.56
+
+
+//3. Boolean -> True or False
+
+
+let a = true;
+let b= `True`;
+console.log
+
+//Null -> it is like you given it null so in future you will give it a real value 
+
+let asd=null;
+console.log(asd)
+
+//Undefined -> it is given by js engine 
+// let num;
+
+// Symbol -> Unique immutable value
+
+
+//future mei hum use karegay labraries, usme hoti hai kuch fields or ab hum be kabhi similar fields bana dede hai or vo fields libraries ki original fileds ko rplace kar deti hai
+
+let u1 = Symbol("uid");
+
+let u2 = Symbol("uid");
+
+
+
+
+
+
