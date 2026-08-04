210f219af6a63dab539f4428d2434564643a3714
VJADOUN2000 <jadounvikrant@gmail.com>
2026-08-04 23:52:47 +0530

    ternary operators

--- Day13 (JS)/script.js
+++ Day13 (JS)/script.js
@@ -25,5 +25,22 @@ console.log(sd);
 
 //&& || !
 
+//to check nature of data !!34 => true 
+
+//unary operator
+
+// + - ! typeof ++ --
+
+// to convert string number to number datatype use like +"89" => become 89
+
+//=======ternary operator=====================
+
+// ?:
+
+
+// Condition ? true wala oce: flase wala code
+
+console.log(23>22?console.log("True"):console.log("False"))
+
 
 
