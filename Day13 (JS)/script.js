7a20f2c5c78b2044343b58b334eaafd75e29ff45
VJADOUN2000 <jadounvikrant@gmail.com>
2026-08-04 23:35:46 +0530

    started operators

---
+++ Day13 (JS)/script.js
@@ -0,0 +1,29 @@
+// Arithmetic, comparsion, logical, assignment, unary, ternary
+
+// Arithmetic -> +, -, /, *, **
+
+//comparsion => ==, ===, !=, !!=,<=, >= 
+
+console.log(23==[23]) // gives true as it does not check data types
+console.log(24===[24]) // gives false 
+
+// != not strict
+// !== strict comparsion
+
+// assigment
+
+let sd = 23;
+
+sd+= 3;
+sd/= 2;
+sd%= 5;
+sd%= 7;
+
+console.log(sd);
+
+//logical  
+
+//&& || !
+
+
+
