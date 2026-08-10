a298c36b788aa3e062e481beba25d8c774d1801b
Vikrant Singh <jadounvikrant@gmail.com>
2026-08-10 23:53:31 +0530

    Practice some simple questions on loop

--- Day15 (JS Loops)/script.js
+++ Day15 (JS Loops)/script.js
@@ -43,3 +43,39 @@ for(let i=5;i<15;i++){
     }
     console.log(i);
 }
+
+// practice question for loops
+
+// Q1. Print number 1 to 10 
+
+for (let a = 1;a<11;a++){
+    console.log(a);
+}
+
+//Q2. Print 1 to 10 using while loop
+
+let b=1;
+while(b<11){
+    console.log(b);
+    b++;
+}
+
+//Q3. Print even number from 1 to 20 using for loop
+
+for(let i=1;i<21;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+//Q4. Print Odd number from 1 to 15 using while loop
+
+let c=1;
+while(i<16){
+    if(i%2==0){
+        //continue;
+    }
+    else{
+        console.log(c);
+    }
+}
