79dd9c4d6af207352d7440edc4b20c0c96027d87
Vikrant Singh <jadounvikrant@gmail.com>
2026-08-10 23:29:30 +0530

    Revise for and while loop

--- Day15 (JS Loops)/script.js
+++ Day15 (JS Loops)/script.js
@@ -5,8 +5,31 @@
 // kaha se jana hai -> kaha tak jana hai -> kaise jana hai
 // for loop
 
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
+
 
 // kaha se jana hai -> kab rukna hai -> kaise jana hai
 // while loop
 
+/*
+start 
+while(){
+  change  
+}*/ 
 
+let i=1;
+while(i<=32){
+    console.log(i)
+    i++;
+}
+
+// do while
+
+let j=12;
+do{
+  console.log(j);
+  j++
+}
+while(j<8);
