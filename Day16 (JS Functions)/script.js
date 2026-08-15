4c775d26c6bd8f6ed96ea5e92bc626e3e3fc48dd
Vikrant Singh <jadounvikrant@gmail.com>
2026-08-15 23:31:19 +0530

    learn about function declaration, expression & fat arrow function

--- Day16 (JS Functions)/script.js
+++ Day16 (JS Functions)/script.js
@@ -12,3 +12,34 @@ function dance(){
 }
 
 dance();
+
+// Different types of function
+
+let fnc = function(){
+    console.log("Function using a variable!");
+}
+
+fnc()
+
+// function declaration, expression and arrow functions
+
+// function declaration :
+
+function func1(){
+    // declaration
+}
+
+// Function expression
+
+let num = function(){
+    // console.log("");
+    // Function expression
+
+}
+
+//fat arrow function
+let num1 = ()=>{
+    console.log("Fat arrow function");
+}
+
+num1();
