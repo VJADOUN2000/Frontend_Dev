f332ee7a51007d59fa91e115f06ec5eb249bb30a
VJADOUN2000 <jadounvikrant@gmail.com>
2026-08-17 23:24:00 +0530

    understand default , rest operators in function

--- Day16 (JS Functions)/script.js
+++ Day16 (JS Functions)/script.js
@@ -61,5 +61,29 @@ function multipy(v1,v2){  // ---> Perameters
 
 multipy(56,65) //--> Arguments
 
+// =============Default perameters===============>
+
+// function add(a,b){ default is undefined
+    function add(a=1,b=7){ // it will use this as default if no value is provided
+   console.log(a+b); 
+}
 
 
+add(); // it will give NaN since undined +undined is not a number
+
+//====================Rest parameters===============>
+
+// jab arguments kai saare ho tu humein utne hi parameters bane padege. issey bachne ke liye, hum rest ka use karte hai ...
+
+// agar ... function ke paramenter mein lage tu vo rest operator hai
+
+// agar ... arrays or object mei lage tu spread operator  hai
+
+
+function abcd(a,b,c,...val){
+  console.log(val)
+
+  console.log(typeof(val))
+}
+
+abcd(34,43,434,65,656,765,7657,657,567,6576,5435,43,32,4234,34);
