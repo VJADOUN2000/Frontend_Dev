4205fae4f580068cc9eacd69151b05cafee0b20f
Vikrant Singh <jadounvikrant@gmail.com>
2026-09-09 00:03:53 +0530

    Learn about shift and unshift in Array

--- Day17 (JS Array)/script.js
+++ Day17 (JS Array)/script.js
@@ -15,8 +15,18 @@ let arr5 = [45,85,6,45,121,48]
 
 // how to push ar add list in array
 
-arr5.push(4000);
+arr5.push(4000);  // Add value in the end of array
 
 console.log(arr5)
 
 arr5.pop();
+
+console.log(arr5);  // Remove last value in array
+
+
+arr5.unshift("Vikrant");  // unshift adds value in the starting of array
+console.log(arr5);
+
+arr5.shift();
+console.log(arr5)  // shift-> Remove first value in the array
+
