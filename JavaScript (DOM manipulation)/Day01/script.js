4a25940f492a6f61c9690b3257286320bf91f58d
Vikrant Singh <jadounvikrant@gmail.com>
2026-09-25 23:56:19 +0530

    Started Dom in JS and practice html se element select

---
+++ JavaScript (DOM manipulation)/Day01/script.js
@@ -0,0 +1,17 @@
+// JS Dom Manipulation !
+
+// html se element select karna
+// text badalna
+// html badalna
+// css badalna
+//attribute
+// event listerners
+
+// html se element select
+
+// 1. document.getelementById()
+// 2. document.queryselector()
+//3. document.getelementsByClassName()
+
+let a = document.getElementsByClassName("adcd")
+console.log(a)
