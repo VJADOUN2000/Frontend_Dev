db58b573e6abd9d01a722d44a830706f94547275
Vikrant Singh <jadounvikrant@gmail.com>
2026-09-26 22:22:10 +0530

    Learn about queryselector and queryselectorAll

--- JavaScript (DOM manipulation)/Day01/script.js
+++ JavaScript (DOM manipulation)/Day01/script.js
@@ -13,5 +13,11 @@
 // 2. document.queryselector()
 //3. document.getelementsByClassName()
 
-let a = document.getElementsByClassName("adcd")
+let a = document.getElementsByClassName("adcd") 
 console.log(a)
+
+let ab = document.querySelector("h1");  //it will slect first element of H1
+console.log(ab.textContent)
+
+let abc = document.querySelectorAll("h1");  //it will slect first element of H1
+console.log(abc)
